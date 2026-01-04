45cbe8a3f28e196ffe93026a8428132d2a187492
Mohamed Aref <maref.dev@gmail.com>
2026-01-04 23:07:43 +0000

    get this error when run sql command
    Error: ERROR: 42601: syntax error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   typescript: {
     ignoreBuildErrors: true,
   },
